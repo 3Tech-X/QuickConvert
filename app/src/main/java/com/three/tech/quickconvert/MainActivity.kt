c4a0c3efdfb7a952479519b090a0abe384cce22e
sshreyas <shreyas.280598@gmail.com>
2026-09-13 14:50:53 +0530

    Added Splash Screen to the app

--- app/src/main/java/com/three/tech/quickconvert/MainActivity.kt
+++ app/src/main/java/com/three/tech/quickconvert/MainActivity.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.three.tech.quickconvert.navigation.QCNavigation
+import com.three.tech.quickconvert.ui.AnimatedSplashScreen
 import com.three.tech.quickconvert.ui.theme.QuickConvertTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,13 +19,24 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
         installSplashScreen().apply {
-
+            setKeepOnScreenCondition { false }
         }
+        
         enableEdgeToEdge()
         setContent {
+            var showSplashScreen by remember { mutableStateOf(true) }
             QuickConvertTheme {
-                QCNavigation(this)
+                if (showSplashScreen) {
+                    AnimatedSplashScreen(
+                        onAnimationComplete = {
+                            showSplashScreen = false
+                        }
+                    )
+                } else {
+                    QCNavigation(this)
+                }
             }
         }
     }
